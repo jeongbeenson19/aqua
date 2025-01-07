03c12d0fae2e6ff3fb9e8ceea93ed35a0533aaff
Jeongbeen Son <jeongbeenson19@gmail.com>
2025-01-07 22:34:29 +0900

    refactor: MongoDB Atlas ssl 인증서 검증 비활성화

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,15 +3,28 @@ from pymongo import MongoClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from urllib.parse import quote_plus
+import ssl
 
 # MongoDB 설정
 password = quote_plus(os.environ['MONGO_PW'])
 MONGO_URI = f"mongodb+srv://jeongbeenson19:{password}@aqua.wchta.mongodb.net/?retryWrites=true&w=majority&appName=aqua"
-mongo_client = MongoClient(MONGO_URI)
+mongo_client = MongoClient(
+    MONGO_URI,
+    ssl=True,
+    ssl_cert_reqs=ssl.CERT_NONE
+)
+
 mongo_db = mongo_client['quiz_db']
 
-# MySQL 설정
+# 환경 변수에서 RDS 접속 정보를 가져옵니다.
+rds_host = os.environ['RDS_HOST']  # 예: mydbinstance.c9akciq32.rds.amazonaws.com
+rds_port = os.environ.get('RDS_PORT', '3306')  # 기본 MySQL 포트 3306
 my_password = os.environ['MYSQL_PW']
-DATABASE_URL = f"mysql+mysqlconnector://root:{my_password}@localhost/user_db"
+db_name = os.environ['RDS_DB_NAME']  # RDS에서 사용 중인 데이터베이스 이름
+
+# RDS에 연결할 DATABASE_URL 생성
+DATABASE_URL = f"mysql+mysqlconnector://root:{my_password}@{rds_host}:{rds_port}/{db_name}"
+
+# SQLAlchemy 엔진 및 세션 생성
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
