08dc5e8f53d366d65335ca7ade5dc3fa29fb2c7c
heejinny <heejin.010702@gmail.com>
2025-03-03 17:15:21 +0900

    프론트엔드 폴더 이름 변경 및 파일 추가

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,281 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+// import { fetchPlotSunburst, fetchMeanScore } from "../pages/mypage";
+import { getItemWithExpiry } from '../pages/auth';
+import axios from 'axios';
+import styles from '../styles/home.module.css';
+import Plot from "react-plotly.js";
+import '../styles/plot.css';
+
+const backendURL = process.env.REACT_APP_BACKEND_URL;
+
+function Home() {
+  const navigate = useNavigate();
+  const [activeTab, setActiveTab] = useState("과목");
+  const [userId, setUserId] = useState(getItemWithExpiry('user_id'));
+  const [token, setToken] = useState(getItemWithExpiry('jwt_token'));
+  const [attemptedNotes, setAttemptedNotes] = useState([]);
+  const [plotData, setPlotData] = useState();
+  const [layout, setLayout] = useState(null);
+  const [scores, setScores] = useState([]);
+
+  // 퀴즈 과목
+  const quizTitles = {
+    SCT: "스포츠 사회학",
+    EDU: "스포츠 교육학",
+    PSY: "스포츠 심리학",
+    HIS: "한국 체육사",
+    PHY: "운동 생리학",
+    KIN: "운동 역학",
+    ETH: "스포츠 윤리",
+  };
+
+  // 탭 버튼 클릭 시 화면 전환
+  const handleTabChange = (tabName) => {
+    setActiveTab(tabName);
+  };
+
+  // 퀴즈 페이지 이동
+  const handleSubjectClick = async (quizType) => {
+    try {
+      if (!token) {
+        alert("로그인 후 이용해 주세요.");
+        navigate('/login');
+        return;
+      }
+      navigate(`/quiz/${quizType}`);
+    } catch (error) {
+      console.error("오류 발생:", error.message);
+    }
+  };
+
+
+  // 마이페이지 차트
+  useEffect(() => {
+    const fetchPlotSunburst = async () => {
+      try {
+        const response = await axios.get(`${backendURL}/my_page/plot/sunburst/${userId}`, {
+        });
+        if (response.status === 200) {
+          const parsedData = JSON.parse(response.data.plot);
+          setPlotData(parsedData);
+          setLayout(prevLayout => {
+            const newLayout = { ...prevLayout };
+
+            newLayout.width = 400;
+            newLayout.height = 400;
+            return newLayout;
+          });
+          console.log(parsedData.layout)
+        }
+        else {
+          alert("마이페이지를 가져오는 데 실패했습니다.");
+        }
+      } catch (error) {
+        console.error("마이페이지 차트 요청 실패 : ", error.message);
+      }
+    }
+    fetchPlotSunburst();
+  }, []);
+
+  // 마이페이지 과목 별 평균 점수
+  useEffect(() => {
+    const fetchMeanScore = async () => {
+      try {
+        const response = await axios.get(`${backendURL}/my_page/mean_score/${userId}`);
+
+        if (response.status === 200) {
+          const { mean_scores } = response.data;
+
+          const updatedScores = Object.keys(quizTitles).map((key) => ({
+            subject: quizTitles[key],
+            score: mean_scores[key] ?? 0,
+          }));
+
+          setScores(updatedScores);
+        } else {
+          alert("마이페이지를 가져오는 데 실패했습니다.");
+        }
+      } catch (error) {
+        console.error("마이페이지 요청 실패 : ", error.message);
+      }
+    };
+
+    fetchMeanScore();
+  }, []);
+
+
+  // 오답노트 리스트
+  useEffect(() => {
+    const fetchAttemptedNotes = async () => {
+      try {
+        const response = await axios.get(`${backendURL}/attempted/${userId}/list`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (response.status === 200) {
+          setAttemptedNotes((response.data.attempted_quiz_sets))
+        } else {
+          alert("오답노트를 가져오는 데 실패했습니다.");
+        }
+      }
+      catch (error) {
+        console.error("오답노트 요청 실패:", error.message);
+      }
+    };
+    fetchAttemptedNotes();
+  }, []);
+
+  // 오답노트 페이지 이동
+  const handleExamReviewClick = async (result_id, quiz_type, quiz_set_id) => {
+    try {
+      navigate(`/examreview/${result_id}`, {
+        state: { result_id, quiz_type, quiz_set_id }
+      });
+    } catch (error) {
+      console.error("오류 발생:", error.message);
+    }
+  };
+
+
+  // 로그아웃
+  const handleLogout = () => {
+    localStorage.removeItem('jwt_token');
+    localStorage.removeItem('user_id');
+
+    navigate('/login');
+  };
+
+  return (
+    <div className={styles.home}>
+      {/* 헤더 */}
+      <header className={styles.header}>
+        <img
+          src='/images/aqua.png'
+          alt="aqua"
+          className={styles.aquaImage}>
+        </img>
+        <h1>2급 생활스포츠지도사</h1>
+      </header>
+
+      {/* 버튼 네비게이션 */}
+      <nav className={styles.tab_btns}>
+        <button
+          className={`${styles.button} ${activeTab === "오답노트" ? styles.active : ""}`}
+          onClick={() => handleTabChange("오답노트")}
+        >
+          오답노트
+        </button>
+        <button
+          className={`${styles.button} ${activeTab === "과목" ? styles.active : ""}`}
+          onClick={() => handleTabChange("과목")}
+        >
+          과목
+        </button>
+        <button
+          className={`${styles.button} ${activeTab === "마이페이지" ? styles.active : ""}`}
+          onClick={() => handleTabChange("마이페이지")}
+        >
+          마이페이지
+        </button>
+
+      </nav>
+
+      {/* 탭에 따라 화면 변경 */}
+      <main className={styles.tab_content}>
+        {activeTab === "과목" && (
+          <div className={styles.subject_ctn}>
+            <button onClick={() => handleSubjectClick("SCT")}>
+              스포츠 사회학
+            </button>
+            <button onClick={() => handleSubjectClick("EDU")}>
+              스포츠 교육학
+            </button>
+            <button onClick={() => handleSubjectClick("PSY")}>
+              스포츠 심리학
+            </button>
+            <button onClick={() => handleSubjectClick("HIS")}>
+              한국 체육사
+            </button>
+            <button onClick={() => handleSubjectClick("PHY")}>
+              운동 생리학
+            </button>
+            <button onClick={() => handleSubjectClick("KIN")}>
+              운동 역학
+            </button>
+            <button onClick={() => handleSubjectClick("ETH")}>
+              스포츠 윤리
+            </button>
+          </div>
+        )}
+
+        {activeTab === "마이페이지" && (
+          <div className={styles.mypage_ctn}>
+            <div className={styles.user_info}>
+              <h2 className={styles.user_name}>이름 : </h2>
+              <h3 className={styles.user_email}>이메일 : </h3>
+            </div>
+
+            <div>
+              {plotData ? (
+                <Plot
+                  data={plotData.data}
+                  layout={layout}
+                  className={styles.plot_container}
+                />
+              ) : (
+                <p className={styles.loading_text}>
+                  차트 로딩 중...
+                </p>
+              )}
+            </div>
+
+            <div className={styles.scores}>
+              <h2 className={styles.scores_title}>과목별 평균 점수</h2>
+              <ul className={styles.scores_list}>
+                {scores.map((item, index) => (
+                  <li key={index}>
+                    <span>{item.subject} :</span>
+                    <span>{item.score}점</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className={styles.actions}>
+              <button className={styles.logout_btn} onClick={handleLogout}>
+                로그아웃
+              </button>
+            </div>
+          </div>
+        )}
+        {activeTab === "오답노트" && (
+          <div className={styles.exam_history_ctn}>
+            {Object.keys(attemptedNotes).length === 0 ? (
+              <p className={styles.noExamMessage}>아직 시험을 풀지 않았습니다. <br />
+                시험을 풀고 나면 <br />
+                오답노트를 확인할 수 있습니다.
+              </p>
+            ) : (
+              <ul className={styles.exam_history_list}>
+                {Object.keys(attemptedNotes).map(key => (
+                  <li key={key}>
+                    <button onClick={() => handleExamReviewClick(
+                      attemptedNotes[key].id,
+                      attemptedNotes[key].quiz_type,
+                      attemptedNotes[key].quiz_set_id
+                    )}>
+                      문제 풀이 {parseInt(key) + 1}회 | {quizTitles[attemptedNotes[key].quiz_type] || "알 수 없음"}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        )}
+      </main >
+    </div >
+  );
+}
+
+export default Home;
---
+++ frontend/src/pages/quiz.js
@@ -0,0 +1,260 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import axios from 'axios';
+import ReportError from '../pages/reportError';
+import { getItemWithExpiry } from '../pages/auth';
+import styles from '../styles/quiz.module.css';
+
+function Quiz() {
+  const backendURL = process.env.REACT_APP_BACKEND_URL;
+  const [userId, setUserId] = useState(getItemWithExpiry('user_id'));
+  const [token, setToken] = useState(getItemWithExpiry('jwt_token'));
+
+  const { subjectId } = useParams();
+  const navigate = useNavigate();
+  const [seconds, setSeconds] = useState(0);
+
+  const [quizData, setQuizData] = useState(null);
+  const [currentQuiz, setCurrentQuiz] = useState(null);
+  const [currentQuizIndex, setCurrentQuizIndex] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const [isReportModalOpen, setReportModalOpen] = useState(false);
+
+  // 퀴즈 데이터
+  useEffect(() => {
+    const fetchQuizData = async () => {
+      try {
+        const quizType = subjectId;
+
+        const response = await axios.get(`${backendURL}/quiz/${quizType}/${userId}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (response.status === 200) {
+          console.log(response.data);
+          console.log(response.data.quiz_set.quiz);
+          // setQuizData(response.data);
+          // setQuizData(response.data.quiz_set.quiz);
+
+        } else {
+          throw new Error("퀴즈 데이터를 불러오는 데 실패했습니다.");
+        }
+      } catch (error) {
+        console.error("오류 발생:", error.message);
+      }
+    };
+    fetchQuizData();
+
+    const timer = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+
+  }, [subjectId]);
+
+  // 현재 퀴즈 번호
+  useEffect(() => {
+    if (quizData && quizData.quiz_set?.quiz.length > 0) {
+      setCurrentQuiz(quizData.quiz_set.quiz[currentQuizIndex]);
+    }
+  }, [quizData, currentQuizIndex]);
+
+  // mm:ss 형식으로 변환
+  const formatTime = (totalSeconds) => {
+    const minutes = Math.floor(totalSeconds / 60);
+    const remainingSeconds = totalSeconds % 60;
+    return `${String(minutes).padStart(2, "0")}:${String(remainingSeconds).padStart(2, "0")}`;
+  };
+
+  // 퀴즈 과목
+  const quizTitles = {
+    SCT: "스포츠 사회학",
+    EDU: "스포츠 교육학",
+    PSY: "스포츠 심리학",
+    HIS: "한국 체육사",
+    PHY: "운동 생리학",
+    KIN: "운동 역학",
+    ETH: "스포츠 윤리",
+  };
+
+  const quizTitle = quizTitles[quizData?.quiz_type] || "알 수 없는 유형";
+  // const correctOptions = quizData?.quiz.map(quiz => quiz.quiz_content.correct_option) || [];
+
+
+  // 답변 선택
+  const handleAnswerSelect = (option) => {
+    const updatedAnswers = [...userAnswers];
+    updatedAnswers[currentQuizIndex] = option;
+    setUserAnswers(updatedAnswers);
+  };
+
+
+  // 다음 문제로 이동
+  const handleNext = () => {
+    if (currentQuizIndex < quizData.quiz_set.quiz.length - 1) {
+      setCurrentQuizIndex((prev) => prev + 1);
+    }
+  };
+
+  // 이전 문제로 이동
+  const handlePrev = () => {
+    if (currentQuizIndex > 0) {
+      setCurrentQuizIndex((prev) => prev - 1);
+    }
+  };
+
+  // 제출 버튼 클릭 시
+  const handleSubmit = async () => {
+    const userConfirmed = window.confirm("제출하시겠습니까?");
+
+    if (!userConfirmed) return;
+
+    try {
+      // 결과 배열 생성
+      const quizResults = quizData.quiz_set.quiz.map((quiz, index) => {
+        const correctOption = quiz.quiz_content.correct_option;
+        const userAnswer = userAnswers[index];
+        const isCorrect = Number(userAnswer) === correctOption ? 1 : 0;
+
+        return {
+          quiz_id: quiz.quiz_id,
+          user_answer: userAnswer,
+          is_correct: isCorrect,
+        };
+      });
+
+      // 최종 서버로 전송할 request 객체 생성
+      const request = {
+        quiz_set_id: quizData.quiz_set.quiz_set_id,
+        quiz_type: quizData.quiz_type,
+        score: quizResults.filter((result) => result.is_correct === 1).length * 5,
+        quiz_results: quizResults,
+      };
+
+      alert("점수 : " + request.score);
+
+      const API_URL = `${process.env.REACT_APP_BACKEND_URL}/quiz/submit/${userId}`;
+
+      const response = await axios.post(API_URL, request, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      alert("퀴즈 제출 완료!");
+
+      navigate(`/`);
+
+    } catch (error) {
+      console.error("퀴즈 제출 오류:", error);
+      alert("퀴즈 제출 중 오류가 발생했습니다.");
+    }
+  }
+
+  const question = currentQuiz?.quiz_content?.question_text || "질문을 불러오는 중입니다. 잠시만 기다려 주세요.";
+  const example = currentQuiz?.quiz_content?.example || [];
+  const options = currentQuiz?.quiz_content?.options || {};
+  // const optionsArray = Object.values(options);
+
+  if (!quizData) {
+    return <div className={styles.waiting}>
+      <h2>퀴즈 데이터를 불러오는 중...</h2>
+    </div>;
+  }
+
+  if (!quizData.quiz_set || !quizData.quiz_set.quiz) {
+    return <div className={styles.waiting}>
+      <h2>존재하지 않거나 잘못된 데이터입니다.<br />
+        다시 시도해주세요.</h2>
+    </div>;
+  }
+
+  return (
+    <div className="quiz">
+      {/* 헤더 */}
+      <header className={styles.header}>
+        <h2 className={styles.timer}>{formatTime(seconds)}</h2>
+        <h1>{quizTitle}</h1>
+        <img
+          src="../images/issue.png"
+          alt="문제 오류 신고"
+          onClick={() => setReportModalOpen(true)}
+        // onClick={handleReportClick}
+        />
+      </header>
+
+      {/* 메인 컨텐츠 */}
+      {/* <main className={styles.exam}>
+        <div className={styles.exam_ctn}>
+          <div className={styles.quiz_ctn}>
+            <p className={styles.question}>{currentQuizIndex + 1}. {question}</p>
+            {Array.isArray(example) && example.length > 0 && (
+              <ul className={styles.example_list}>
+                {example.map((item, index) => (
+                  <li key={index} className={styles.example_item}>
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+          <div className={styles.options_ctn}>
+            {Object.entries(options)?.map(([key, option], index) => (
+              <button
+                key={key}
+                className={
+                  userAnswers[currentQuizIndex] === key
+                    ? styles.selected_option
+                    : styles.option
+                }
+                onClick={() => handleAnswerSelect(key)}
+              >
+                {`${key}. ${option}`}
+              </button>
+            ))}
+          </div>
+        </div> */}
+
+      {/* 내비게이션 버튼 */}
+      {/* <div className={styles.nav_ctn}> */}
+      {/* <div className={`${styles.nav_ctn} 
+    ${currentQuizIndex === 0 || currentQuizIndex === quizData.quiz_set.quiz.length
+          ? styles.single_button : styles.double_buttons}`}>
+
+        {currentQuizIndex > 0 && (
+          <button onClick={handlePrev} className={styles.nav_btn}>
+            이전
+          </button>
+        )}
+        {currentQuizIndex < quizData.quiz_set.quiz.length - 1 && (
+          <button onClick={handleNext} className={styles.nav_btn}>
+            다음
+          </button>
+        )}
+        {currentQuizIndex === quizData.quiz_set.quiz.length - 1 && (
+          <button onClick={handleSubmit} className={styles.submit_btn}>
+            제출
+          </button>
+        )}
+      </div>
+    </main> */}
+
+      <ReportError
+        isOpen={isReportModalOpen}
+        onClose={() => setReportModalOpen(false)}
+      // quiz_type={quiz_type}
+      // quiz_set_id={quiz_set_id}
+      // quiz_id={currentQuizIndex}
+      />
+
+    </div >
+  );
+}
+
+export default Quiz;
