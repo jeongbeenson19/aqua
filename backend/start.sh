d433ecbc0363e95652c93cba5fc0758fd1355f63
Jeongbeen Son <jeongbeenson19@gmail.com>
2025-03-06 14:39:26 +0900

    Modify: 아 좀 되라 하

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,8 @@
-nginx &
+#!/bin/bash
 
-# FastAPI 서버 실행
+# Nginx 실행 (백그라운드 모드)
+nginx
+
+# Nginx가 완전히 실행된 후 FastAPI 서버 실행
+sleep 2  # Nginx가 시작되는 시간을 기다립니다
 uvicorn app.main:app --host 0.0.0.0 --port 8000
